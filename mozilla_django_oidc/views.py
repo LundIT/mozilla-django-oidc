0417f9bf5177f7b1b91ccc5645bfee8a13d5d01b
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2025-07-04 15:37:04 +0200

    save session after login callback

--- mozilla_django_oidc/views.py
+++ mozilla_django_oidc/views.py
@@ -74,6 +74,8 @@ class OIDCAuthenticationCallbackView(View):
         if "expires_in" in tok:
             self.request.session["oidc_access_token_expiration"] = time.time() + tok["expires_in"]
 
+        self.request.session.save()
+
         return HttpResponseRedirect(self.success_url)
 
     def get(self, request):
